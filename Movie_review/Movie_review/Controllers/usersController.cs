e4360620169f5c41a2b39a9941139199a317292f
Chinmay <Chinmay@CHINMAY>
2017-03-31 23:48:33 +0530

    Controllers added to  and database updated on azure

---
+++ Movie_review/Movie_review/Controllers/usersController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Movie_review.Models;
+
+namespace Movie_review.Controllers
+{
+    public class usersController : ApiController
+    {
+        private WebServiceEntities db = new WebServiceEntities();
+
+        // GET: api/users
+        public IQueryable<user> Getusers()
+        {
+            return db.users;
+        }
+
+        // GET: api/users/5
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Getuser(string id)
+        {
+            user user = await db.users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/users/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putuser(string id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/users
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Postuser(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (userExists(user.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/users/5
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Deleteuser(string id)
+        {
+            user user = await db.users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            await db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool userExists(string id)
+        {
+            return db.users.Count(e => e.Id == id) > 0;
+        }
+    }
+}
